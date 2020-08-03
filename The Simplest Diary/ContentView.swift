c5bdf02610cb8658f9656a6fd799cca50bf9c685
webunreal <nikolaiivanovcontact@gmail.com>
2020-08-03 22:22:13 +0300

    Initial Commit

---
+++ The Simplest Diary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Simplest Diary
+//
+//  Created by Nikolai Ivanov on 03.08.2020.
+//  Copyright © 2020 Nikolai Ivanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Simplest DiaryTests/The_Simplest_DiaryTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Simplest_DiaryTests.swift
+//  The Simplest DiaryTests
+//
+//  Created by Nikolai Ivanov on 03.08.2020.
+//  Copyright © 2020 Nikolai Ivanov. All rights reserved.
+//
+
+import XCTest
+@testable import The_Simplest_Diary
+
+class The_Simplest_DiaryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
